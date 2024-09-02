249133b54090a02b91ebd399039f7e48ee49a8d9
notfound313 <marufmuh666@gmail.com>
2024-09-02 21:42:53 +0700

    add queue consume on music app

---
+++ src/PlaylistService.js
@@ -0,0 +1,44 @@
+const { Pool } = require('pg')
+
+class PlaylistService {
+  constructor() {
+    this._pool = new Pool()
+  }
+
+  async getPlaylistById(playlistId) {
+    const query = {
+      text: 'SELECT id, name FROM playlist WHERE id=$1',
+      values: [playlistId],
+    }
+
+
+    const result = await this._pool.query(query)
+    
+    return result.rows[0]
+  }
+
+  async getSongsByPlaylistId(playlistId) {
+    const query = {
+      text: `SELECT song.song_id, song.title, song.performer
+             FROM song
+             INNER JOIN playlist_song ON song.song_id = playlist_song.song_id
+             WHERE playlist_song.playlist_id = $1`,
+      values: [playlistId],
+    }
+
+    const result = await this._pool.query(query)
+    return result.rows
+  }
+
+  async getPlaylistWithSongs(playlistId) {
+    const playlist = await this.getPlaylistById(playlistId)
+    const songs = await this.getSongsByPlaylistId(playlistId)
+
+    return {
+      ...playlist,
+      songs,
+    }
+  }
+}
+
+module.exports = PlaylistService
---
+++ src/listener.js
@@ -0,0 +1,27 @@
+
+const PlaylistService = require('./PlaylistService');
+const MailSender = require('./MailSender');
+
+class Listener {
+  constructor(playlistService, mailSender) {
+    this._playlistService = playlistService;
+    this._mailSender = mailSender;
+
+    this.listen = this.listen.bind(this);
+  }
+
+  async listen(message) {
+    try {
+      const { playlistId, targetEmail } = JSON.parse(message.content.toString());
+      console.log(playlistId)
+
+      const playlist = await this._playlistService.getPlaylistWithSongs(playlistId);
+      const result = await this._mailSender.sendEmail(targetEmail, JSON.stringify(playlist));
+      console.log(result);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+}
+
+module.exports = Listener;
